e039b75c8d596471700e8ff61c26658721ffdef3
JEONGMIN <gyflsakfn@naver.com>
2022-10-10 22:21:18 +0900

    Date move

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,35 @@
+import { useContext, useState } from "react";
+import { TodoStateContext } from "../App";
 import Button from "../components/Button";
 import DateInfo from "../components/DateInfo";
 import Header from "../components/Header";
 
 const Home = () => {
+  // Context의 TodoState를 불러옴
+  const todoList = useContext(TodoStateContext);
+  console.log(todoList);
+  // 날짜의 상태
+  const [curDate, setCurDate] = useState(new Date());
+  // 현재 날짜 추출
+  const dateText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
+
+  // 1달 증가
+  const increaseMonth = () => {
+    setCurDate(new Date(curDate.getFullYear(), curDate.getMonth() + 1));
+  };
+
+  // 1달 감소
+  const decreaseMonth = () => {
+    setCurDate(new Date(curDate.getFullYear(), curDate.getMonth() - 1));
+  };
+
   return (
     <div>
       <Header headText={"헤더텍스트"} />
       <DateInfo
-        dateText={"데이트텍스트"}
-        rightChild={<Button text={"오른쪽 자식"} />}
-        leftChild={<Button text={"왼쪽 자식"} />}
+        dateText={dateText}
+        leftChild={<Button text={"<"} onClick={decreaseMonth} />}
+        rightChild={<Button text={">"} onClick={increaseMonth} />}
       />
     </div>
   );
